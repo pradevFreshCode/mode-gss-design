946cc4e6eab66e612bafacada2c6004da0faa5f6
Viktor <viktor.lytvyn@freshcode.org>
2018-06-08 18:14:17 +0300

    old invalid google maps api key replaced with valid

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   assets_dir: '/assets/',
   api_url: 'http://api.gosweetspot.com/api/',
-  agm_key: 'AIzaSyDCvpg954JraP5HVSncoIA_FNIcKwLOGGY',
+  agm_key: 'AIzaSyBG86bkQnVviHzukLqYMXbnPD6ELdu26vs',
   stripe_pk_key: 'pk_test_nKiObmbzObQwdu0Yjqq3Tz2H',
   stripe_sk_key: 'sk_test_Np3ugz2TvTNyFtgiITM8lNQf',
   stripe_api_url: 'https://api.stripe.com/v1/charges',
